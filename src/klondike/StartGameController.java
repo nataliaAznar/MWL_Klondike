3177508d8bc46233bff048f32f7c11712be32b66
nataliaAznar <natalia.aznar@opendeusto.es>
2015-03-28 13:40:22 +0100

    Subida del proyecto

---
+++ src/klondike/StartGameController.java
@@ -0,0 +1,35 @@
+package klondike;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class StartGameController {
+
+	public int sizeWaste() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<Integer> sizeFoundations() {
+		ArrayList<Integer> sizeFoundations = new ArrayList<Integer>();
+		for (int i = 0; i < 4; i++) {
+			sizeFoundations.add(0);
+		}
+		return sizeFoundations;
+	}
+
+	public int sizeDeck() {
+		return 24;
+	}
+
+	public ArrayList<Integer> sizeCoveredCardsTableaus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Stack<Card>> uncoveredCardsStackTableaus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/klondike/StartGameControllerTest.java
@@ -0,0 +1,57 @@
+package klondike;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StartGameControllerTest {
+
+	private StartGameController startGameController;
+	
+	@Before
+	public void before(){
+		startGameController = new StartGameController();
+	}
+	@Test
+	public void sizeWastetest() {
+		assertEquals(0,startGameController.sizeWaste());
+	}
+
+	@Test
+	public void sizeFoundationsTest() {
+		ArrayList<Integer> sizeFoundations = startGameController.sizeFoundations();
+		assertEquals(4, sizeFoundations.size());
+		for(int sizeFoundation: sizeFoundations){
+			assertEquals(0, sizeFoundation);
+		}
+	}
+	
+	@Test
+	public void sizeDeckTest() {
+		assertEquals(24,startGameController.sizeDeck());
+	}
+	
+	@Test
+	public void sizeTableausTest() {
+		ArrayList<Integer> sizeTableaus = startGameController.sizeCoveredCardsTableaus();
+		ArrayList<Stack<Card>> uncoveredCardsStackTableaus= startGameController.uncoveredCardsStackTableaus();
+		assertEquals(7, sizeTableaus.size());
+		assertEquals(7, uncoveredCardsStackTableaus.size());
+		
+		for(int i = 0; i < sizeTableaus.size(); i++)
+			assertEquals(new Integer(i+1), sizeTableaus.get(i));
+		
+		for(Stack<Card> uncoveredCardsStack : uncoveredCardsStackTableaus){
+			assertEquals(1, uncoveredCardsStack.size());
+			assertTrue(uncoveredCardsStack.peek().uncovered());
+		}
+	
+	}
+	
+	
+}
+
